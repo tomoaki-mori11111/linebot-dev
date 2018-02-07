140aa68734dd031837bf99cefdf47f373b1c1105
tomoaki-mori11111 <36126374+tomoaki-mori11111@users.noreply.github.com>
2018-02-07 23:18:55 +0900

    Update server.js

--- server.js
+++ server.js
@@ -5,26 +5,26 @@ const https = require('https');
 const crypto = require('crypto');
 const Twitter = require('twitter');
 const tw = new Twitter({
-// Twitterの4つのキーを指定
-  consumer_key: 'dR3eWhizC9aloijwMF8WwpIv7', // Consumer Keyを記述
-  consumer_secret: 'WhkCcinF8HIvZEZfymTRplZ231usgHhpxUHMwg6KMp1cTYKuOX', // Consumer Secretを記述
-  access_token_key: '542173666-krGq8D6LROv6mWfrIUOy3J6dUiG4Ll8Cb9MhhJuN', // Access Tokenを記述
-  access_token_secret: 'QmZAx5VPEJT709IlEAE6gy6KYqEmW0tL3nWoIBw3MiJZc' // Access Token Secretを記述
+// 4つのキーの指定
+  consumer_key: process.env.CONSUMER_KEY, // Consumer Key
+  consumer_secret: process.env.CONSUMER_SECRET, // Consumer Secret
+  access_token_key: process.env.ACCESS_TOKEN_KEY, // Access Token
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET // Access Token Secret
 });
 const TARGET_HASHTAG = '#TrainDelay';
 
 const HOST = 'api.line.me';
-const CH_SECRET = 'c092cd24c7204931d2e0409dd57926f1'; // Channel Secretを記述
-const CH_ACCESS_TOKEN = 'sT+g87Zf9no4a9MRMs6jgozryK412Rz3ghoj25cxAXxVioRtcsnJrOeGarlPUR/iyutFMjxLKsXFqKPsEqVWXP9tpO8SD6wbo4s8pEng9gnVwTIJqAix/xXcoLI/mfJVryspaXS+ObudeNVlhQ8G4AdB04t89/1O/w1cDnyilFU='; // Channel Access Tokenを記述
-const USER_ID = 'U21a4f0968fb4fcb6d9a8741316b45537'; // Your userIdを記述
+const CH_SECRET = process.env.LINE_CH_SECRET; // Channel Secret
+const CH_ACCESS_TOKEN = process.env.LINE_CH_ACCESS_TOKEN; // Channel Access Token
+const USER_ID = process.env.LINE_USER_ID;// Your userId
 const PUSH_PATH = '/v2/bot/message/multicast';
 const SIGNATURE = crypto.createHmac('sha256', CH_SECRET);
-const PORT = 3000;
+const PORT = process.env.PORT || 3000; // ||で条件分岐
 
 const pushClient = (userId, SendMessageObject) => {
     let postDataStr = JSON.stringify({ to: userId, messages: SendMessageObject });
     let options = {
-(        host: HOST,
+        host: HOST,
         port: 443,
         path: PUSH_PATH,
         method: 'POST',
@@ -58,63 +58,7 @@ const pushClient = (userId, SendMessageObject) => {
 
 tw.stream('statuses/filter', {'track': TARGET_HASHTAG}, (stream) => {
   stream.on('data', (data) => {
-    if (data.text.indexOf('南武線') !== -1) {
-        let PushSendMessageObject = [{
-          type: 'text',
-          text: data.text
-        }];
-
-        pushClient([USER_ID], PushSendMessageObject)
-          .then((body) => {
-            console.log(body);
-          }, (e) => {console.log(e)});
-
-        console.log(data.text);
-    }
-    
-     if (data.text.indexOf('横須賀線') !== -1) {
-        let PushSendMessageObject = [{
-          type: 'text',
-          text: data.text
-        }];
-
-        pushClient([USER_ID], PushSendMessageObject)
-          .then((body) => {
-            console.log(body);
-          }, (e) => {console.log(e)});
-
-        console.log(data.text);
-    }
-
-    if (data.text.indexOf('湘南新宿ライン') !== -1) {
-        let PushSendMessageObject = [{
-          type: 'text',
-          text: data.text
-        }];
-
-        pushClient([USER_ID], PushSendMessageObject)
-          .then((body) => {
-            console.log(body);
-          }, (e) => {console.log(e)});
-
-        console.log(data.text);
-    }
-
-    if (data.text.indexOf('東横線') !== -1) {
-        let PushSendMessageObject = [{
-          type: 'text',
-          text: data.text
-        }];
-
-        pushClient([USER_ID], PushSendMessageObject)
-          .then((body) => {
-            console.log(body);
-          }, (e) => {console.log(e)});
-
-        console.log(data.text);
-    }
-
-    if (data.text.indexOf('目黒線') !== -1) {
+    if (data.text.indexOf('上野東京ライン') !== -1) {
         let PushSendMessageObject = [{
           type: 'text',
           text: data.text
