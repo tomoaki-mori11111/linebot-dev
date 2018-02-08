b028c993afa41f59c43bff63ec622459951c59c7
tomoaki-mori11111 <36126374+tomoaki-mori11111@users.noreply.github.com>
2018-02-08 09:09:55 +0900

    Update server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ const pushClient = (userId, SendMessageObject) => {
 
 tw.stream('statuses/filter', {'track': TARGET_HASHTAG}, (stream) => {
   stream.on('data', (data) => {
-    if (data.text.indexOf('上野東京ライン') !== -1) {
+    if (data.text.indexOf('TrainDelay') !== -1) {
         let PushSendMessageObject = [{
           type: 'text',
           text: data.text
